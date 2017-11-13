a919738f03ab995a11d3629358a304c78b4b9acd
John Ryan <jpr269@cornell.edu>
2017-11-13 10:51:16 -0500

    wallcollisions

---
+++ verify/wall-collisions.cpp
@@ -0,0 +1,24 @@
+Clockwise spin 
+Rad vector -0.837242 -0.546833
+Original disc velocity -1.000000 -1.000000
+ Original wall velocity -0.500000 -0.000000
+Ang vel was 0.000000
+Stationary boundary frame now
+ Normal vel -0.808318 -0.527942
+Tangent vel 0.308318 -0.472058
+Angv is now -0.187942
+New tangent vector 0.102773 -0.157353
+New normal vector 0.808318 0.527942
+
+
+Counter Clockwise spin
+Rad vector 0.733032 -0.680194
+Original disc velocity 0.000000 -2.000000
+ Original wall velocity 0.250000 0.433013
+Ang vel was 0.000000
+Stationary boundary frame now
+ Normal vel 1.078776 -1.001015
+Tangent vel -1.328776 -1.431998
+Angv is now 0.651175
+New tangent vector -0.442925 -0.477333
+New normal vector -1.078776 1.001015
